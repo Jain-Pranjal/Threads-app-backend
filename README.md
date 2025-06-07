d7308b2f213d87857df4826ce08f50a91985add4
Pranjal Jain <pranjalworkon@gmail.com>
2025-06-07 19:42:42 +0530

    feat: setup the apollo sever

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ npx tsc --init    # create tsconfig.json
 yarn add tsc-watch -D  # to compile
 
 - now change the in and out directories in tsconfig.json
+
+using the [express middleware]({https://www.apollographql.com/docs/apollo-server/api/express-middleware#expressmiddleware}) to connect Apollo Server with Express
+
+- we have use the apolo server in the same port as the express server but to access the apollo server we need to use the /graphql endpoint 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,66 @@
 import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@as-integrations/express5';
+
+import cors from 'cors';
+
+
+async function init(){
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, World!'
-  });
+
+
+// making the apollo server
+const gqlServer = new ApolloServer({
+    typeDefs: `
+    # This is a simple GraphQL schema
+    type Myhello{
+        message: String
+        user: String
+        roll: Int
+    }
+
+    type Query {
+        hello: Myhello
+        say(name: String!, age: Int!): String
+    }
+    `,
+
+    resolvers: {
+        Query: {
+            hello: () => ({
+                message: 'Hello, World from GraphQL!',
+                user: 'John Doe',
+                roll: 1
+            }),
+            say: (_: any, args: { name: string , age: number }) => {
+                return `Hello, ${args.name}! You are ${args.age} years old.`;
+            }
+
+        }
+    }
 });
+
+
+// Start the Apollo server
+await gqlServer.start();
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, World!'
+    });
+});
+
+// routing for GraphQL
+app.use(
+  '/graphql',
+  express.json(),
+  cors<cors.CorsRequest>(),
+  expressMiddleware(gqlServer),
+);
+// at /graphql it will open the UI of GraphQL playground
 
 
 
@@ -13,3 +68,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+}
+
+// calling the main function to start the server
+init();
